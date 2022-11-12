bd019d0d2799d8ed05f7acdd3f45c123179c015b
Leonardo Marques <leonardomsilva@MacBook-Air-de-Leonardo.local>
2022-11-12 15:27:02 -0300

    novas alteracoes

--- app/src/main/java/com/example/dicastp01/MainActivity.kt
+++ app/src/main/java/com/example/dicastp01/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.dicastp01
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.dicastp01.data.RegistroPeso
 
 import com.example.dicastp01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val retornoActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult())
+    { activityResult ->
+        if (activityResult.resultCode == RESULT_OK) {
+            activityResult.data?.let {
+                if (it.hasExtra(TAG)) {
+                    val retorno = if (Build.VERSION.SDK_INT >= 33) {
+                        it.getParcelableExtra(TAG, RegistroPeso::class.java)
+                    } else {
+                        it.getParcelableExtra(TAG)
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding    = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/example/dicastp01/data/RegistroPeso.kt
+++ app/src/main/java/com/example/dicastp01/data/RegistroPeso.kt
@@ -1,7 +1,7 @@
 package com.example.dicastp01.data
 
+public data class RegistroPeso {
+    val peso: Double = 0.0
+    val faixaEtaria: Int = 0
 
-data class RegistroPeso {
-    val peso: Double,
-    val faixaEtaria: Int
 }
